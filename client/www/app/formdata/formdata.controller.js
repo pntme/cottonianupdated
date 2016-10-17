6529261b6e69718ce24fa4aaa717b225152f7efe
prabhu <prabhunath877@gmail.com>
2016-10-17 10:03:29 +0530

    designing pages

--- client/www/app/formdata/formdata.controller.js
+++ client/www/app/formdata/formdata.controller.js
@@ -2,8 +2,72 @@
     'use strict';
     angular.module('cot').controller('FormdataCtrl', FormdataCtrl);
 
-    function FormdataCtrl($state, $localStorage, Image,  $ionicLoading , $ionicModal, tostService, $scope, ajaxRequest, localStorageService) {
+    function FormdataCtrl($state, $localStorage, Image,  $ionicLoading , $stateParams ,
+    	$ionicModal, tostService, $scope, ajaxRequest, localStorageService) {
         var self = this;
+        var FinalApi, FinalFile;
+        var user = localStorageService.get('UserData')[0].email;
+        console.log($stateParams)
+        self.navtitle = $stateParams.FormTitle;
+        var FinalApi = $stateParams.api;
+         $scope.publish = function() {
+            $ionicLoading.show();
+            var title = document.getElementById('FormTitle').value;
+            var description = document.getElementById('FormDesc').value;
+            if (FinalFile)
+                FileUpload(FinalFile, FinalApi, title, description);
+            else {
+                ajaxRequest.send(FinalApi, {
+                    title: title,
+                    description: description,
+                    user: user
+                }, 'POST').then(function(res) {
+                    $ionicLoading.hide();
+                    tostService.notify('Uploaded successfully', 'top');
+                });
+
+            }
+        }
+
+
+        $scope.select = function() {
+            Image.takePhoto1('Profile photo').then(function(blob) {
+                FinalFile = blob;
+                self.UserPic = "data:image/jpeg;base64," + Image.binary;
+            });
+        }
+
+        function FileUpload(blob,  title, description) {
+         var query = Image.upload({
+                file: blob   
+               });
+               query.then(function(data) {
+                  if(data.status == 200 && data.statusText == "OK"){
+                    var filename = data.data.replace(/(\r\n|\n|\r)/gm,"");
+                       ajaxRequest.send(FinalApi, {
+                          title: title,
+                          description: description,
+                          user: user,
+                          image : filename  
+                       }, 'POST').then(function(res) {
+                            $scope.hide();
+                            $ionicLoading.hide();
+                            tostService.notify('Submitted successfully', 'top');
+                      });
+                  }
+                  else{
+                            $scope.hide();
+                            $ionicLoading.hide();                   
+                    tostService.notify('Failed to submit, Please try again', 'top'); 
+                  }
+               }).catch(function(e){
+                 console.log(e);
+                                            $scope.hide();
+                            $ionicLoading.hide();
+                  tostService.notify('Failed to submit, Please try again', 'top');
+               })
+        }
+
 
     }
 
